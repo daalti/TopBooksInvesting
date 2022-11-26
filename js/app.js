1bc45bf669887c7cd7e5ac51274febe5332489b4
daalti <daaltigo@gmail.com>
2022-11-26 18:18:44 +0100

    AddingTraidingStrategyPage
    Adding Traiding Strategy Page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 const btnMenu = document.querySelector(".header .header-container .btn-menu");
 const menu = document.querySelector(".header .header-container .nav");
+const close = document.querySelector(".close");
+const body = document.querySelector("body");
+const categoryMain = document.querySelector(
+  ".header .header-container .nav .category"
+);
+const category = document.querySelector(
+  ".header .header-container .nav .item-category"
+);
+const categoryItem = document.querySelector(
+  ".header .header-container .nav .menu-item-container"
+);
 
 btnMenu.addEventListener("click", () => {
   menu.classList.toggle("activo");
+  categoryMain.classList.toggle("activo");
+  body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+  menu.classList.toggle("activo");
+  categoryMain.classList.toggle("activo");
+  body.style.overflow = "auto";
+  categoryItem.classList.remove("activo");
+});
+
+category.addEventListener("click", () => {
+  categoryItem.classList.toggle("activo");
 });
