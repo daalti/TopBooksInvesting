04e39dcdaed83a547e2306fa1c1cd8c658bc7f33
daalti <daaltigo@gmail.com>
2022-11-27 16:18:55 +0100

    AddingAuthor
    Missing a lot of things like header in a lot of pages..

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ const body = document.querySelector("body");
 const categoryMain = document.querySelector(
   ".header .header-container .nav .category"
 );
-const category = document.querySelector(
+const category = document.querySelectorAll(
   ".header .header-container .nav .item-category"
 );
-const categoryItem = document.querySelector(
+const categoryItem = document.querySelectorAll(
   ".header .header-container .nav .menu-item-container"
 );
 
@@ -22,9 +22,23 @@ close.addEventListener("click", () => {
   menu.classList.toggle("activo");
   categoryMain.classList.toggle("activo");
   body.style.overflow = "auto";
-  categoryItem.classList.remove("activo");
+  categoryItem[0].classList.remove("activo");
+  categoryItem[1].classList.remove("activo");
+  categoryItem[2].classList.remove("activo");
+  categoryItem[3].classList.remove("activo");
 });
 
-category.addEventListener("click", () => {
-  categoryItem.classList.toggle("activo");
+category.forEach(function (item) {
+  item.addEventListener("click", function () {
+    item_category = item.querySelector("p");
+    if (item_category.textContent == "Category") {
+      categoryItem[0].classList.toggle("activo");
+    } else if (item_category.textContent == "Author") {
+      categoryItem[1].classList.toggle("activo");
+    } else if (item_category.textContent == "Company") {
+      categoryItem[2].classList.toggle("activo");
+    } else if (item_category.textContent == "Best Seller") {
+      categoryItem[3].classList.toggle("activo");
+    }
+  });
 });
